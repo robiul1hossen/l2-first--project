bf4eccdf1feb5c2289023f7d63f8a0fcc7fd42ca
Mohammad Robiul Hossen <robim6225@gmail.com>
2023-11-20 23:15:00 +0600

    created interface and module seccussfully

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+export type UserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  contactNo: string;
+  occupation: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: 'A' | 'B' | 'AB' | 'O';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
